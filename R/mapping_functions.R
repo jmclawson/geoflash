ad699e08fcb03fa6dc6ebb2d8d4c84bd2b538c08
James Clawson <clawson@gmail.com>
2024-10-08 11:35:40 -0500

    cleaner flow, improved labels, improved colors

--- R/mapping_functions.R
+++ R/mapping_functions.R
@@ -1,9 +1,10 @@
 library(tidyverse)
-library(geojsonsf)
 library(sf)
 library(leaflet)
 
-load_geojson <- function(url = "https://data.cityofchicago.org/api/geospatial/bbvz-uum9?method=export&format=GeoJSON") {
+load_geojson <- function(url = "https://data.cityofchicago.org/api/geospatial/bbvz-uum9?method=export&format=GeoJSON", label = pri_neigh) {
+  # limit color palette and try to avoid
+  # neighboring regions of same-color
   add_colors <- function(df) {
     geo_colors <- df |>
       st_touches(sparse = TRUE) |>
@@ -11,12 +12,29 @@ load_geojson <- function(url = "https://data.cityofchicago.org/api/geospatial/bb
       igraph::graph_from_adjacency_matrix() |>
       igraph::greedy_vertex_coloring("dsatur")
 
-    df |>
+    df <- df |>
       mutate(
         fill_colors = geo_colors
       )
+
+    lincoln_sq <- df |>
+      filter(neighborhood == "Lincoln Square") |>
+      pull(fill_colors)
+
+    df |>
+      mutate(
+        # optimize bad / unideal solutions
+        fill_colors = case_when(
+          neighborhood %in% c("Chinatown", "Mckinley Park", "Kenwood", "Englewood", "Printers Row", "Oakland, Kenwood", "Woodlawn", "Museum Campus", "Armour Square, Chinatown") ~ 4, # dark
+          neighborhood == "Wrigleyville" ~ lincoln_sq,
+          neighborhood == "Loop" ~ 1, # red
+          neighborhood %in% c("Millenium Park", "Near South Side") ~ 3, # green
+          TRUE ~ fill_colors
+        )
+      )
   }
 
+  # download data if it doesn't exist
   file <- "data/shapefile.geojson"
 
   if (!dir.exists("data")) {
@@ -27,37 +45,63 @@ load_geojson <- function(url = "https://data.cityofchicago.org/api/geospatial/bb
     download.file(url, file, quiet = TRUE)
   }
 
-  geojson_sf(file) |>
-    add_colors()
-}
+  df <- geojsonsf::geojson_sf(file)
 
-make_sf_map <- function(df = geo_shapes, label = pri_neigh, portion = c("all", "north", "central", "south"), split = NULL, hide_ohare = FALSE) {
-  portion <- match.arg(portion)
   df <- df |>
+    rename(neighborhood = {{ label }}) |>
     mutate(
-      y = st_coordinates(suppressWarnings(st_point_on_surface(geometry)))[, 2],
-      region = case_when(
-        pri_neigh %in% c("West Town", "Gold Coast") ~ "central",
-        y >= quantile(y, probs = seq(0,1,0.33))[3] ~ "north",
-        y >= quantile(y, probs = seq(0,1,0.33))[2] ~ "central",
-        TRUE ~ "south",
-      ),
-      {{ label }} := {{ label }} |>
+      # sec_neigh is in all caps
+      neighborhood = neighborhood |>
         str_to_title() |>
         str_replace_all("Uic", "UIC") |>
         str_replace_all("O'hare", "O'Hare") |>
         str_replace_all("Ohare", "O'Hare") |>
         str_replace_all("Mckin", "McKin") |>
         str_replace_all("Depaul", "DePaul") |>
+        str_replace_all("nters ", "nter's ") |>
         str_replace_all(",", ", ") |>
+        str_replace_all(" Of ", " of ") |>
+        str_replace_all(" The ", " the ") |>
+        str_replace_all(" And ", " and ") |>
+        str_replace_all(
+          "Kenwood, Oakland",
+          "Oakland, Kenwood") |>
+        str_replace_all(
+          "Craigin", "Cragin") |>
         str_squish()
+    ) |>
+    group_by(neighborhood) |>
+    summarise(geometry = st_union(geometry)) |>
+    ungroup()
+
+  df |>
+    add_colors()
+}
+
+make_sf_map <- function(df = geo_shapes, portion = c("all", "north", "central", "south"), split = NULL, hide_ohare = FALSE, palette = paletteer::paletteer_d("rtist::hopper")) {
+  portion <- match.arg(portion)
+
+  df <- df |>
+    mutate(
+      # get a polygon's midpoint
+      y = st_coordinates(suppressWarnings(st_point_on_surface(geometry)))[, 2],
+      # divide city in three regions
+      region = case_when(
+        neighborhood %in% c("West Town", "Gold Coast") ~ "central",
+        neighborhood %in% c("Old Town") ~ "north",
+        y >= quantile(y, probs = seq(0,1,0.33))[3] ~ "north",
+        y >= quantile(y, probs = seq(0,1,0.33))[2] ~ "central",
+        TRUE ~ "south"
+      )
     )
 
   if (hide_ohare) {
     df <- df |>
-      filter({{ label }} != "O'Hare")
+      filter(neighborhood != "O'Hare")
   }
 
+  # split allows arbitrary divisions
+  # currently just shows areas north of line
   if (!is.null(split)) {
     portion <- "all"
     df <- df |>
@@ -87,11 +131,6 @@ make_sf_map <- function(df = geo_shapes, label = pri_neigh, portion = c("all", "
       st_intersection(x, st_as_sfc(st_bbox(df)))
       )}()
 
-  df <- df |>
-    group_by({{ label }}, fill_colors) |>
-    summarise(geometry = st_union(geometry)) |>
-    ungroup()
-
   geo_centroids <- suppressWarnings(st_point_on_surface(df))
 
   df <- df |>
@@ -103,25 +142,22 @@ make_sf_map <- function(df = geo_shapes, label = pri_neigh, portion = c("all", "
     ggplot() +
     geom_sf(data = df_roads,
             color = "gray") +
-    geom_sf(aes(fill = LETTERS[fill_colors]),# {{ label }}),
-            alpha = 0.4,
+    geom_sf(aes(fill = LETTERS[fill_colors]),
+            alpha = 0.6,
             show.legend = FALSE) +
     geom_sf(data = df_water,
             fill = "skyblue",
             color = NA) +
     ggrepel::geom_label_repel(
-      aes(label = {{ label }},
-          # fill = LETTERS[fill_colors],
+      aes(label = neighborhood,
           geometry = label_placement),
-      # show.legend = FALSE,
-      # alpha = 0.3,
       stat = "sf_coordinates",
       size = 1.7,
       min.segment.length = 0.3
     ) +
     theme_void() +
     scale_fill_manual(
-      values = paletteer::paletteer_d("rtist::hopper")[1:4]
+      values = palette[1:4]
     )
 }
 
@@ -135,13 +171,14 @@ make_leaflet <- function(df, palette = paletteer::paletteer_d("rtist::hopper")) 
       col = "black",
       weight = 1,
       fillOpacity = 0.5,
-      popup = df$pri_neigh)
+      popup = df$neighborhood)
 }
 
 ##### Use: #####
 # load_geojson() |>
 #   make_leaflet()
 #
-# load_geojson() |>
-#   make_sf_map(portion = "central")
+load_geojson() |>
+  make_sf_map(portion = "central",
+              palette = paletteer::paletteer_d("wesanderson::Royal1"))
 
