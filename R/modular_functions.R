ac1cf7e20d82189a1fd44521ca31e13316a64c1b
James Clawson <clawson@gmail.com>
2024-10-14 13:19:15 -0500

    polished version

--- R/modular_functions.R
+++ R/modular_functions.R
@@ -2,13 +2,22 @@ library(tidyverse)
 library(sf)
 library(leaflet)
 
-load_geojson <- function(url = "https://data.cityofchicago.org/api/geospatial/bbvz-uum9?method=export&format=GeoJSON") {
-  # limit color palette and try to avoid
-  # neighboring regions of same-color
-
+# csv url for testing: https://data.cityofnewyork.us/api/views/7t3b-ywvw/rows.csv?accessType=DOWNLOAD
+load_geo_file <- function(url = "https://data.cityofchicago.org/api/geospatial/bbvz-uum9?method=export&format=GeoJSON", type = NULL) {
+  if (is.null(type)) {
+    if (str_detect(tolower(url), "geojson")) {
+      type <- "geojson"
+    } else if (str_detect(tolower(url), "csv")) {
+      type <- "csv"
+    }
+  }
 
   # download data if it doesn't exist
-  file <- "data/shapefile.geojson"
+  the_name <- url |>
+    urltools::domain() |>
+    str_replace_all("[.]", "_") |>
+    paste0(".", type)
+  file <- fs::path("data", the_name)
 
   if (!dir.exists("data")) {
     dir.create("data")
@@ -18,7 +27,22 @@ load_geojson <- function(url = "https://data.cityofchicago.org/api/geospatial/bb
     download.file(url, file, quiet = TRUE)
   }
 
-  geojsonsf::geojson_sf(file)
+  if (type == "csv") {
+    result <- file |>
+      st_read(options = c("GEOM_POSSIBLE_NAMES=*geom*")) |>
+      st_set_crs(4326)
+  } else if (type == "geojson") {
+    result <- geojsonsf::geojson_sf(file)
+  } else {
+    print("File type is unclear.")
+  }
+
+  if (!"geometry" %in% colnames(result)) {
+    result <- result |>
+      rename(geometry = colnames(result)["sfc_MULTIPOLYGON" == unlist(map(result, class))])
+  }
+
+  return(result)
 }
 
 choose_label <- function(df, label = pri_neigh) {
@@ -65,13 +89,17 @@ add_colors <- function(df) {
 optimize_chicago_colors <- function(df) {
   lincoln_sq <- df |>
     filter(neighborhood == "Lincoln Square") |>
-    pull(fill_colors)
+    pull(fill_colors) |>
+    {\(x) ifelse(length(x) == 0, 1, x)}()
+
+  # return(df)
 
   df |>
     mutate(
       # optimize bad / unideal solutions
       fill_colors = case_when(
-        neighborhood %in% c("Chinatown", "Mckinley Park", "Kenwood", "Englewood", "Printers Row", "Oakland, Kenwood", "Woodlawn", "Museum Campus", "Armour Square, Chinatown") ~ 4, # dark
+        neighborhood %in% c("Chinatown", "Mckinley Park","McKinley Park", "Kenwood", "Englewood", "Printers Row", "Oakland, Kenwood", "Woodlawn", "Museum Campus", "Armour Square, Chinatown") ~ 4, # dark
+        str_detect(tolower(neighborhood),"mckinley park") ~ 4,
         neighborhood == "Wrigleyville" ~ lincoln_sq,
         neighborhood == "Loop" ~ 1, # red
         neighborhood %in% c("Millenium Park", "Near South Side") ~ 3, # green
@@ -194,30 +222,21 @@ choose_region <- function(df, portion, split_x = NULL, split_y = NULL) {
   df
 }
 
+make_sf_map <- function(df = geo_shapes, palette = paletteer::paletteer_d("rtist::hopper"), label = TRUE, show_accuracy = FALSE) {
+  states_df <- usmap::us_map("states") |>
+    st_transform(crs = 4326)
 
-make_sf_map <- function(df = geo_shapes, hide_ohare = FALSE, palette = paletteer::paletteer_d("rtist::hopper")) {
-  if (hide_ohare) {
-    df <- df |>
-      filter(neighborhood != "O'Hare")
-  }
+  states_abbr <- df |>
+    st_join(states_df) |>
+    pull(abbr) |>
+    unique()
 
-  df_roads <- suppressMessages(tigris::primary_secondary_roads(
-    state = "IL",
-    filter_by = st_bbox(df),
-    progress_bar = FALSE)) |>
-    st_transform(st_crs(df)) |>
-    {\(x) suppressWarnings(
-      st_intersection(x, st_as_sfc(st_bbox(df)))
-    )}()
+  counties_df <- usmap::us_map("counties", include = states_abbr) |>
+    st_transform(crs = 4326)
 
-  df_water <- suppressMessages(tigris::area_water(
-    state = "IL",
-    county = "Cook",
-    progress_bar = FALSE)) |>
-    st_transform(st_crs(df)) |>
-    {\(x) suppressWarnings(
-      st_intersection(x, st_as_sfc(st_bbox(df)))
-      )}()
+  df_roads <- get_roads(df)
+
+  df_water <- get_water(df)
 
   geo_centroids <- suppressWarnings(st_point_on_surface(df))
 
@@ -226,27 +245,113 @@ make_sf_map <- function(df = geo_shapes, hide_ohare = FALSE, palette = paletteer
       label_placement = geo_centroids$geometry
     )
 
-  df |>
+  the_plot <- df |>
     ggplot() +
     geom_sf(data = df_roads,
-            color = "gray") +
-    geom_sf(aes(fill = LETTERS[fill_colors]),
-            alpha = 0.6,
-            show.legend = FALSE) +
+            color = "gray")
+
+  if (!show_accuracy) {
+    the_plot <- the_plot +
+      geom_sf(aes(fill = LETTERS[fill_colors]),
+              alpha = 0.6,
+              show.legend = FALSE)
+  } else {
+    the_plot <- the_plot +
+      geom_sf(aes(fill = accuracy),
+              alpha = 0.6)
+  }
+  the_plot <- the_plot +
     geom_sf(data = df_water,
             fill = "skyblue",
-            color = NA) +
-    ggrepel::geom_label_repel(
-      aes(label = neighborhood,
-          geometry = label_placement),
-      stat = "sf_coordinates",
-      size = 1.7,
-      min.segment.length = 0.3
-    ) +
-    theme_void() +
-    scale_fill_manual(
-      values = palette[1:4]
+            color = NA)
+
+  if (label) {
+    the_plot <- the_plot +
+      ggrepel::geom_label_repel(
+        aes(label = neighborhood,
+            geometry = label_placement),
+        stat = "sf_coordinates",
+        size = 6,
+        min.segment.length = 0.3
+      )
+  }
+
+  if (!show_accuracy) {
+    the_plot <- the_plot +
+      scale_fill_manual(
+        values = palette[1:4]
+      )
+  } else {
+    the_plot <- the_plot +
+      scale_fill_viridis_c(option = "magma")
+  }
+  the_plot +
+    theme_void()
+}
+
+get_named_intersects <- function(df) {
+  states_df <- usmap::us_map("states") |>
+    st_transform(crs = st_crs(df))
+
+  state_intersect <- df |>
+    st_join(states_df) |>
+    pull(abbr) |>
+    unique()
+
+  counties_df <-
+    usmap::us_map(
+      "counties",
+      include = state_intersect) |>
+    st_transform(crs = st_crs(df))
+
+  the_df <- df |>
+    st_join(counties_df) |>
+    mutate(
+      county = county |>
+        str_remove_all("County") |>
+        trimws()
     )
+
+  data.frame(
+    state = the_df$abbr,
+    county = the_df$county) |>
+    distinct()
+}
+
+get_roads <- function(df) {
+  inner_roads <- function(df, state) {
+    suppressMessages(tigris::primary_secondary_roads(
+      state = state,
+      filter_by = st_bbox(df),
+      progress_bar = FALSE)) |>
+      st_transform(st_crs(df)) |>
+      {\(x) suppressWarnings(
+        st_intersection(x, st_as_sfc(st_bbox(df)))
+      )}()
+  }
+
+  unique(get_named_intersects(df)$state) |>
+    map({\(x) inner_roads(df, x)}) |>
+    list_rbind() |>
+    st_as_sf()
+}
+
+get_water <- function(df) {
+  inner_water <- function(df, state, county) {
+    suppressMessages(tigris::area_water(
+      state = state,
+      county = county,
+      progress_bar = FALSE)) |>
+      st_transform(st_crs(df)) |>
+      {\(x) suppressWarnings(
+        st_intersection(x, st_as_sfc(st_bbox(df)))
+      )}()
+  }
+
+  get_named_intersects(df) |>
+    pmap(inner_water, df = df) |>
+    list_rbind() |>
+    st_as_sf()
 }
 
 make_leaflet <- function(df, palette = paletteer::paletteer_d("rtist::hopper")) {
@@ -262,11 +367,24 @@ make_leaflet <- function(df, palette = paletteer::paletteer_d("rtist::hopper")) 
       popup = df$neighborhood)
 }
 
-##### Use: #####
-# load_geojson() |>
-#   make_leaflet()
-#
-# load_geojson() |>
-#   make_sf_map(portion = "central",
-#               palette = paletteer::paletteer_d("wesanderson::Royal1"))
+# sf equivalent of brushedPoints for polygons
+brushedMapPoints <- function(df_sf, brush, cache = FALSE) {
+  if (cache) {
+    saveRDS(brush, "~/Developer/geoflash/data/brushPoints.RDS")
+  }
+  the_x <- unlist(brush[1:2])
+  the_y <- unlist(brush[3:4])
+  the_all <- data.frame(
+    longitude = the_x[c(1, 2, 2, 1, 1)],
+    latitude = the_y[c(1, 1, 2, 2, 1)])
 
+  brush_polygon <- the_all |>
+    as.matrix() |>
+    list() |>
+    st_polygon() |>
+    st_sfc(crs = st_crs(df_sf))
+
+  st_intersection(
+    df_sf,
+    brush_polygon)
+}
--- app.R
+++ app.R
@@ -1,48 +1,122 @@
 library(shiny)
 library(bslib)
+library(ragg)# fix font size on labels
 source("R/modular_functions.R")
+
+palette_choices <-
+  paletteer::palettes_d_names |>
+  filter(length >= 4) |>
+  mutate(
+    output = paste(package, palette, sep = "::")
+  ) |>
+  slice_sample(n = 10) |>
+  pull(output) |>
+  c("rtist::hopper", "wesanderson::Royal1") |>
+  unique() |>
+  sort()
 
 ##### UI components ######
 options_sidebar <- sidebar(
-  # p("This is where I'll put options for loading a data set, setting a palette, and choosing regions."),
-  shiny::selectInput(
-    "choose_column", "Select neighborhood column",
-    choices = c("pri_neigh"),
-    selected = "pri_neigh"
+  accordion(
+    id = "side_accordion",
+    multiple = FALSE,
+    open = "Focus",
+    accordion_panel(
+      title = "Load",
+      icon = icon("cloud"),
+      textInput(
+        "choose_url", "URL of GeoJSON or CSV file with spatial boundaries",
+        value = "https://data.cityofchicago.org/api/geospatial/bbvz-uum9?method=export&format=GeoJSON"
+      ),
+      shiny::actionButton(
+        "load_button", "Start mapping",
+        icon = icon("route")
+      )
+    ),
+    accordion_panel(
+      title = "Select",
+      icon = icon("table-columns"),
+      selectInput(
+        "choose_column", "Select neighborhood column",
+        choices = c("Choose" = "")
+      )
+    ),
+    accordion_panel(
+      title = "Focus",
+      icon = icon("magnifying-glass"),
+      selectInput(
+        "choose_region", "Select region",
+        choices = c("all"),
+        selected = "all"
+      )
+    ),
+    accordion_panel(
+      title = "Customize",
+      icon = icon("palette"),
+      selectInput(
+        "choose_palette", "Choose a palette",
+        choices = palette_choices,
+        selected = "rtist::hopper"
+      )
+    )
   ),
-  shiny::selectInput(
-    "choose_region", "Select region",
-    choices = c("all"),
-    selected = "all"
-  ),
-  shiny::selectInput(
-    "choose_palette", "Choose a palette",
-    choices = c("rtist::hopper", "wesanderson::Royal1"),
-    selected = "rtist::hopper"
-  )
-  )
+  uiOutput("page_explanation")
+)
 
 page_learn <- nav_panel(
   "Learn",
-  plotOutput("learn_map")
-  )
+  fillRow(
+    height = "20%",
+    layout_columns(
+      height = "100%",
+      value_box(
+        title = "Region",
+        value = textOutput("learn_region"),
+        showcase = icon("map-location-dot")),
+      value_box(
+        title = "Neighborhoods",
+        value = textOutput("learn_count"),
+        showcase = icon("hashtag"),
+        theme = "secondary"),
+      value_box(
+        title = "Portion of city",
+        value = textOutput("learn_percent"),
+        showcase = icon("chart-pie"),
+        theme = "secondary")
+    )
+  ),
+  card(plotOutput("learn_map"))
+)
 
 page_study <- nav_panel(
-  "Study",
+  "Explore",
   leafletOutput("study_map")
-  )
+)
 
-page_clusters <- nav_panel(
-  "Clusters",
-  p("The idea here is to try k-means clustering to get to know areas by groups of neighborhood names. I think this may be close to how we tend to think of neighborhoods, by relating, for instance, neighborhood X as being near neighborhoods A, B, and C."))
+page_test <- nav_panel(
+  "Test",
+  fillRow(
+    height = "20%",
+    layout_columns(
+      height = "100%",
+      actionButton(
+        inputId="refreshbutton",
+        label = textOutput("the_target"),
+        icon = icon("puzzle-piece"),
+        class = "card"),
+      value_box(
+        title = "Selected",
+        value = textOutput("the_selected"),
+        showcase = icon("object-group")),
+      uiOutput("the_status"))
+  ),
+  card(fill = TRUE,
+       plotOutput("test1", brush = "test_brush"))
+)
 
-page_test_1 <- nav_panel(
-  "Test - 1",
-  p("On this page, users will be shown a map with one neighborhood named. The user will use the brush stroke to select the neighborhood. A status bar or value box will let them know how close they're getting based on which neighborhoods have been selected: If the target neighborhood represents 0% of the selected neighborhoods, it's red with an accompanying bad icon; if it represents greater than 0% of the selected neighborhoods, the color jumps to a scale between white and green, with an accompanying middling icon; as the percentage approaches 100%, it's an affirming green color with a smiley face."))
-
-page_test_2 <- nav_panel(
-  "Test - 2",
-  p("On this page, one neighborhood will be colored and the rest will be blank. The user has to choose the neighborhood's name out of an incomplete list of possible neighborhoods. Optional control: A sliding scale lets the user choose difficulty level; this control will vary the selection of possible neighborhoods, possibly adding additional options or limiting the selection to those neighborhoods that are especially nearby."))
+page_review <- nav_panel(
+  "Review",
+  card(plotOutput("review_plot_map")))
 
 link_source <- tags$a(
   shiny::icon("github"), "Source",
@@ -54,29 +128,74 @@ link_source <- tags$a(
 ##### UI object #####
 
 ui <- page_navbar(
-  title = "GeoFlash",
+  id = "active_tab",
+  title = "FlashCartes",
+  theme = bs_theme(
+    bootswatch = "cosmo", version = 5,
+  ) |>
+    bs_add_rules(
+      list(
+        ".navbar.navbar-default {background-color: $secondary !important;}",
+        "#refreshbutton {height: 100%;}",
+        "#refreshbutton i {font-size: 40pt; margin-bottom: 0.3em; display: block; margin-top: 8%; margin-left: auto; margin-right: auto;}",
+       '#refreshbutton:hover i::before {content: "\\f021";}',
+        "#the_target {font-size: 2rem !important; text-transform: none !important; display: block; margin-left: auto; margin-right: auto;}",
+        "#the_status .card {height: 100%;}")),
   sidebar = options_sidebar,
   page_learn,
   page_study,
-  page_clusters,
-  page_test_1,
-  page_test_2,
+  page_test,
+  page_review,
   nav_spacer(),
   nav_item(link_source)
 )
 
-##### Server object #####
+##### Server function #####
 
 server <- function(input, output, session) {
-  init_data <- reactive({
-    load_geojson()
+  # Try to load something at start
+  app_params <- reactiveValues(
+    the_url = "https://data.cityofchicago.org/api/geospatial/bbvz-uum9?method=export&format=GeoJSON",
+    the_brushed = NA,
+    neighborhood = NA,
+    attempts = NA,
+    accuracy = NA
+  )
+
+  the_tally <- reactive(
+    data.frame(
+      neighborhood = app_params$neighborhood,
+      attempts = app_params$attempts,
+      accuracy = app_params$accuracy
+    )
+  )
+
+  output$page_explanation <- renderUI({
+    if (input$active_tab == "Learn") {
+      div(p("Set a region to focus more closely on one part of the city."), p("URLs for other cities (right click and copy URL):", a("Boston",href="https://hub.arcgis.com/api/v3/datasets/2ac87de59a2c47ca8e6d44da598b8832_79/downloads/data?format=geojson&spatialRefId=4326&where=1%3D1"), ", ", a("Chicago", href = "https://data.cityofchicago.org/api/geospatial/bbvz-uum9?method=export&format=GeoJSON"), ", ", a("New York City", href = "https://data.cityofnewyork.us/api/views/7t3b-ywvw/rows.csv?accessType=DOWNLOAD")))
+    } else if (input$active_tab == "Explore") {
+      p("Zoom in and move the map to identify cross streets and areas of interest.")
+    } else if (input$active_tab == "Test") {
+      div(p("Click and drag your mouse cursor to select ", strong(target_neighborhood()),
+        "as your target. Click the puzzle piece to choose another, or ", shiny::actionLink("refreshlink", "refresh here.")))
+    } else {
+      p("Check the accuracy of your recent testing. Neighborhoods with deeper colors might need to be studied further.")
+    }
   })
 
-  data <- reactive({
-    init_data() |>
-      choose_label(input$choose_column) |>
-      add_colors() |>
-      add_regions()
+  observeEvent(input$load_button,{
+    app_params$the_url = input$choose_url
+  })
+
+  observeEvent(input$choose_url,{
+    updateSelectInput(
+      session, "choose_column",
+      choices = c("Choose" = "")
+    )
+  })
+
+  init_data <- reactive({
+    load_geo_file(app_params$the_url)
   })
 
   column_options <- reactive({
@@ -86,29 +205,263 @@ server <- function(input, output, session) {
       str_subset("shape_len", negate = TRUE)
   })
 
-  output$learn_map <- renderPlot(
+  observe(
+    updateSelectInput(
+      session, "choose_column",
+      choices = column_options(),
+      selected = column_options()[1]
+    )
+  )
+
+  data <- reactive({
+    req(input$choose_column)
+    req(input$choose_column != "geometry")
+    req(input$choose_column != "such_trouble")
+    init_data() |>
+      choose_label(input$choose_column) |>
+      add_colors() |>
+      add_regions()
+  })
+
+  ready_data <- reactive({
+    # print(data())
+    req(input$choose_region)
     data() |>
       choose_region(
-        portion = input$choose_region) |>
+        portion = input$choose_region)
+  })
+
+  output$learn_table <- renderTable(
+    data() |>
+      select(neighborhood, portion) |>
+      as.data.frame() |>
+      head()
+  )
+
+  output$learn_text <- renderText(
+    paste0(
+      "typeof = ",
+      typeof(as.data.frame(init_data())),
+      "; class = ",
+      class(as.data.frame(init_data())) |>
+        paste0(collapse = ", "),
+      "; with dim = ",
+      dim(as.data.frame(init_data())) |>
+        paste0(collapse = " by "))
+  )
+
+  output$learn_region <- renderText(
+    str_to_title(input$choose_region)
+  )
+
+  output$learn_count <- renderText(
+    ready_data() |>
+      nrow()
+  )
+
+  output$learn_percent <- renderText(
+    (nrow(ready_data()) / nrow(data()) * 100) |>
+      round(1) |>
+      paste0("%")
+  )
+
+  output$learn_map <- renderPlot(
+    ready_data() |>
       make_sf_map(
-        palette = paletteer::paletteer_d(input$choose_palette)
+        palette = paletteer::paletteer_d(input$choose_palette),
+        label = TRUE
       )
   )
 
   output$study_map <- renderLeaflet(
-    data() |>
-      choose_region(
-        portion = input$choose_region) |>
+    ready_data() |>
       make_leaflet(
         palette = paletteer::paletteer_d(input$choose_palette)
       )
   )
 
-  observe(
-    updateSelectInput(
-      session, "choose_column",
-      choices = column_options()
-    )
+  target_neighborhood <- reactive({
+    ready_data() |>
+      pull(neighborhood) |>
+      sample(1)
+  })
+
+  output$the_target <- renderText(
+    target_neighborhood()
+  )
+
+  the_brush <- reactive({
+    input$test_brush
+  })
+
+  target_neighborhood <- reactive({
+    input$refreshbutton
+    input$refreshlink
+    ready_data() |>
+      pull(neighborhood) |>
+      sample(1)
+  })
+
+  observeEvent(
+    input$choose_region, {
+      app_params$neighborhood <-
+        app_params$neighborhood |>
+        c(target_neighborhood())
+      app_params$attempts <-
+        app_params$attempts |>
+        c(0)
+      app_params$accuracy <-
+        app_params$accuracy |>
+        c(NA)
+    })
+
+  observeEvent(input$refreshbutton, {
+    session$resetBrush("test_brush")
+    app_params$neighborhood <-
+      app_params$neighborhood |>
+      c(target_neighborhood())
+    app_params$attempts <-
+      app_params$attempts |>
+      c(0)
+    app_params$accuracy <-
+      app_params$accuracy |>
+      c(NA)
+    # print(the_tally())
+    })
+
+  observeEvent(input$refreshlink, {
+    session$resetBrush("test_brush")
+    app_params$neighborhood <-
+      app_params$neighborhood |>
+      c(target_neighborhood())
+    app_params$attempts <-
+      app_params$attempts |>
+      c(0)
+    app_params$accuracy <-
+      app_params$accuracy |>
+      c(NA)
+    # print(the_tally())
+  })
+
+  plus_attempt <- function() {
+    app_params$attempts[length(app_params$attempts)] <- app_params$attempts[length(app_params$attempts)] + 1
+  }
+
+  avg_accuracy <- function() {
+    num_attempts <- app_params$attempts[length(app_params$attempts)]
+
+    current_accuracy <- ifelse(
+      target_neighborhood() %in% brushed_neighborhoods()$neighborhood,
+      1/nrow(brushed_neighborhoods()),
+      0)
+
+    the_mean <-
+      # approximate past attempts
+      rep(app_params$accuracy[length(app_params$accuracy)],
+          length = (num_attempts - 1)) |>
+      # add current attempt
+      c(current_accuracy) |>
+      # average
+      mean(na.rm = TRUE)
+
+    # update param
+    app_params$accuracy[length(app_params$accuracy)] <- the_mean
+  }
+
+  observeEvent(input$test_brush, {
+    plus_attempt()
+    avg_accuracy()
+    output$the_status <- renderUI(
+      if (!isTruthy(input$test_brush)) {
+        value_box(
+          title = "No selection",
+          value = "Select",
+          showcase = icon("arrow-pointer"),
+          theme = "primary"
+        )
+      } else if (isTruthy(brushed_neighborhoods()) && nrow(brushed_neighborhoods()) == 1 &&
+                 brushed_neighborhoods()$neighborhood == target_neighborhood()) {
+        value_box(
+          title = "Status",
+          value = "You did it!",
+          showcase = icon("face-smile-beam"),
+          theme = "success")
+      } else if (isTruthy(brushed_neighborhoods()) && target_neighborhood() %in% brushed_neighborhoods()$neighborhood && 1/nrow(brushed_neighborhoods()) >= 0.3) {
+        value_box(
+          title = "Status",
+          value = "Almost!",
+          showcase = icon("face-surprise"),
+          theme = "cyan"
+        )
+      }  else if (isTruthy(brushed_neighborhoods()) && target_neighborhood() %in% brushed_neighborhoods()$neighborhood && 1/nrow(brushed_neighborhoods()) > 0) {
+        value_box(
+          title = "Status",
+          value = "Meh",
+          showcase = icon("face-meh"),
+          theme = "warning"
+        )
+      } else {
+        value_box(
+          title = "Status",
+          value = "Try again",
+          showcase = icon("face-sad-tear"),
+          theme = "danger"
+        )
+      })
+    })
+
+  brushed_neighborhoods <- reactive({
+    req(input$test_brush)
+    # brushedPoints() doesn't work for sf
+    # brushedPoints(ready_data(), input$test_brush)
+    brushedMapPoints(
+      ready_data(),
+      input$test_brush,
+      cache = FALSE)
+  })
+
+  output$the_selected <- renderText(
+    if (!isTruthy(input$test_brush)) {
+      0
+    } else {
+      nrow(brushed_neighborhoods())
+    }
+  )
+
+  output$the_status <- renderUI(
+    value_box(
+      title = "No selection",
+      value = "Select",
+      showcase = icon("arrow-pointer"),
+      theme = "primary"
+    ))
+
+  output$test1 <- renderPlot(
+    ready_data() |>
+      make_sf_map(
+        palette = paletteer::paletteer_d(input$choose_palette),
+        label = FALSE
+      )
+  )
+
+  review_map <- reactive(
+    ready_data() |>
+      filter(neighborhood %in% the_tally()$neighborhood) |>
+      left_join(
+        the_tally() |>
+          drop_na() |>
+          summarize(
+            accuracy = mean(accuracy, na.rm = TRUE),
+            .by = neighborhood
+            ))
+  )
+
+  output$review_plot_map <- renderPlot(
+    review_map() |>
+      make_sf_map(
+        label = TRUE,
+        show_accuracy = TRUE
+      )
   )
 
   observe(
@@ -117,7 +470,6 @@ server <- function(input, output, session) {
       choices = c("all", unique(pull(data(), region)))
     )
   )
-
 }
 
 ##### Run the application #####
